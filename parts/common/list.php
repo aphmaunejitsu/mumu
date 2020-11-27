97cc7200670aef5960c3396989ba74926067b94a
Aphamu Nejitsu <aphmaunejits@icloud.com>
2020-11-27 15:18:08 +0900

    remove fontawesome and web fonts

--- parts/common/list.php
+++ parts/common/list.php
@@ -3,11 +3,17 @@
 	$search_query = esc_attr( get_search_query() );
 	?>
 	<section class="sgn-category-title center">
-		<h2><i class="fas fa-search"></i> <?php echo $search_query; ?></h2>
+        <h2>
+            <svg width="16" height="16" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="search" class="svg-inline--fa fa-search fa-w-16" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path fill="currentColor" d="M505 442.7L405.3 343c-4.5-4.5-10.6-7-17-7H372c27.6-35.3 44-79.7 44-128C416 93.1 322.9 0 208 0S0 93.1 0 208s93.1 208 208 208c48.3 0 92.7-16.4 128-44v16.3c0 6.4 2.5 12.5 7 17l99.7 99.7c9.4 9.4 24.6 9.4 33.9 0l28.3-28.3c9.4-9.4 9.4-24.6.1-34zM208 336c-70.7 0-128-57.2-128-128 0-70.7 57.2-128 128-128 70.7 0 128 57.2 128 128 0 70.7-57.2 128-128 128z"></path></svg>
+            <?php echo $search_query; ?>
+        </h2>
 	</section>
 <?php elseif ( is_archive() ) : ?>
 	<section class="sgn-category-title center">
-		<h2><i class="fas fa-tags"></i> <?php esc_html( single_cat_title() ); ?></h2>
+        <h2>
+            <?php get_template_part('part/svg/tag'); ?>
+            <?php esc_html( single_cat_title() ); ?>
+        </h2>
 	</section>
 <?php endif; ?>
 <div class="sgn-list">
@@ -53,11 +59,19 @@ if ( have_posts() ) :
 								height='<?php echo $image['height']; ?>' >
 							</amp-img>
 						</div>
-						<div class="post-category nowrap absolute display-inline"><i class="fas fa-tags"></i> <?php echo esc_attr( $category ); ?></div>
+                        <div class="post-category nowrap absolute display-inline">
+                            <?php get_template_part('parts/svg/tag'); ?>
+                            <span><?php echo esc_attr( $category ); ?></span>
+                        </div>
 						<h3 class="post-title mb1 px1"><?php echo $title; ?></h3>
 						<div class="post-time px1">
-							<time datetime="<?php echo $date; ?>" class="mr1"><i class="fas fa-calendar-alt fa-fw"></i> <?php echo $date; ?></time>
-							<time datetime="<?php echo $mdate; ?>"><i class="fas fa-sync-alt fa-fw"></i> <?php echo $mdate; ?></time>
+                            <time datetime="<?php echo $date; ?>" class="mr1">
+                                <?php get_template_part('parts/svg/calender'); ?>
+                                <span><?php echo $date; ?></span>
+                            </time>
+                            <time datetime="<?php echo $mdate; ?>">
+                                <?php get_template_part('parts/svg/refresh'); ?>
+                                <?php echo $mdate; ?></time>
 						</div>
 						<div class="post-content px1"><?php echo $content; ?></div>
 					</div>
