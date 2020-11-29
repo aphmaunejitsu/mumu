9e57a2165ff7cbe4544e49f8c71ee6f74c860553
aphmau nejitsu <aphmau_nejitsu@icloud.com>
2020-11-29 23:55:43 +0900

    add archive

---
+++ archive.php
@@ -0,0 +1,26 @@
+<?php
+get_header(); ?>
+<div id="primary" class="mx-auto">
+    <main id="main">
+        <header class="page-header">
+            <?php
+            the_archive_title( '<h2 class="page-title">', '</h2>' );
+            the_archive_description( '<div class="archive-description">', '</div>' );
+            ?>
+        </header><!-- .page-header -->
+    <?php
+    if (have_posts()) :
+        while (have_posts()) :
+            the_post();
+
+            get_template_part( 'parts/content', get_post_type() );
+        endwhile;
+    else :
+        get_template_part( 'parts/content', 'none' );
+    endif;
+    ?>
+    </main><!-- #main -->
+</div> <!-- #primary -->
+<?php
+get_sidebar();
+get_footer();
