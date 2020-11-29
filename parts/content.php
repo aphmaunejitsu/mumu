8a235675627b0e2fe7e199dee5a97284966a7d58
aphmau nejitsu <aphmau_nejitsu@icloud.com>
2020-11-29 14:28:46 +0900

    deal with autoload

--- parts/content.php
+++ parts/content.php
@@ -1,1 +1,32 @@
-<p><?php echo esc_html(get_the_title()); ?></p>
+<?php
+?>
+<article id="post-<?php the_ID()?>" <?php post_class() ?>>
+    <header class="article-header">
+        <?php
+            if ( is_singular() ) :
+                the_title('<h1 class="entry-title">', '</h1>');
+            else :
+                the_title('<h3 class="entry-title">', '</h3>');
+            endif;
+        ?>
+
+        <?php get_template_part('parts/common/feature-image'); ?>
+        <div class="meta">
+            <time datetime="" class="mr1">
+                <?php get_template_part('parts/svg/calender'); ?>
+                <span><?php echo get_the_date(null, get_the_ID()) ?></span>
+            </time>
+            <time datetime="">
+                <?php get_template_part('parts/svg/refresh'); ?>
+                <span><?php echo get_the_modified_date(null, get_the_ID()) ?></span>
+            </time>
+        </div>
+    </header>
+    <div class="article-content content">
+        <?php the_excerpt() ?>
+    </div>
+    <footer class="article-fotter">
+        <?php the_category(' '); ?>
+        <?php the_tags('', ' '); ?>
+    </footer>
+</article>
