ad3b8b32ff8e8d21a709cb5b5b63a5222ed2f964
Aphamu Nejitsu <aphmaunejits@icloud.com>
2020-12-04 20:59:38 +0900

    add ad sanitizer

--- app/sanitizer.php
+++ app/sanitizer.php
@@ -2,30 +2,29 @@
 
 use Masterminds\HTML5;
 
-require_once dirname(dirname(__FILE__)) . '/classes/content/ad.php';
-require_once dirname(dirname(__FILE__)) . '/classes/content/embed.php';
-require_once dirname(dirname(__FILE__)) . '/classes/content/iframe.php';
-require_once dirname(dirname(__FILE__)) . '/classes/content/image.php';
+require_once MUMU_APP . '/sanitizers/Ad.php';
+require_once MUMU_DIR . '/classes/content/embed.php';
+require_once MUMU_DIR . '/classes/content/iframe.php';
+require_once MUMU_DIR . '/classes/content/image.php';
 
 function amp_content( $content ) {
     if ( ! ( in_the_loop() && is_main_query() ) ) {
         return $content;
     }
 
-	$should_do_sanitize = ['Content_Image', 'Content_Iframe', 'Content_Embed', 'Content_Ad'];
-    $content = do_shortcode(get_the_content());;
-    $html5   = new HTML5();
-    $dom     = $html5->loadHTML( $content );
+    $html = mb_convert_encoding($content, 'HTML-ENTITIES', 'auto');
 
-    foreach ( $should_do_sanitize as $sanitizer ) {
-        $class = new $sanitizer( $dom );
-        $dom   = $class->sanitize();
-    }
+    $html5 = new HTML5();
+    $dom = @$html5->loadHTML($content);
 
-    $html = $dom->saveHTML( $dom );//->nodeValue;
+
+    $dom = (new Ad($dom))();
+    return $dom->saveHTML($dom);
+
     $html = preg_replace( '/<html .*?>/', '', $html );
     $html = preg_replace( '/<\/html>/', '', $html );
     $html = preg_replace( '/<!DOCTYPE html>/', '', $html );
     return $html;
 }
+
 add_filter('the_content', 'amp_content', 999999999);
