fd28239807830778dba7475b880b4ae0d8d5d3ca
aphmau nejitsu <aphmau_nejitsu@icloud.com>
2020-12-05 16:44:18 +0900

    add change amp twitter and youtube

---
+++ app/sanitizers/blocks/Youtube.php
@@ -0,0 +1,68 @@
+<?php
+
+class Youtube
+{
+    const YOUTUBE = 'amp-youtube';
+    public $content;
+    public $nodes;
+
+    public function __construct($content, $nodes)
+    {
+        $this->content = $content;
+        $this->nodes = $nodes;
+    }
+
+    public function __invoke()
+    {
+        _log('Start Youtube::__invoke');
+
+        try {
+            foreach ($this->nodes as $node) {
+                _log($node);
+                if (($id = $this->getYoutubeId($node)) === null) {
+                    _log('nullpo');
+                    continue;
+                }
+
+                $youtube = $this->content->createElement(self::YOUTUBE);
+                $youtube->setAttribute('data-videoid', $id);
+                $youtube->setAttribute('width', 640);
+                $youtube->setAttribute('height', 360);
+                $youtube->setAttribute('layout', 'responsive');
+
+                $node->replaceChild($youtube, $node->firstChild);
+            }
+
+            return $this->content;
+        } catch (\Exception $e) {
+            return $this->content;
+        }
+
+        _log('End Youtube::__invoke');
+    }
+
+    private function getYoutubeId($node)
+    {
+        _log('Start getYoutubeId');
+        $src = $this->content->saveHTML($node);
+		$is_short = false;
+		if ( ! preg_match( '/https:\/\/www\.youtube\.com/', $src ) ) {
+			//https://youtu.be/BTe6Oh-mxDQ
+			if ( ! preg_match( '/https:\/\/youtu.be/', $src ) ) {
+				return null;
+			}
+		}
+
+		if ( ! preg_match( '/.*\/embed\/([^&]+)\?.*/', $src, $match ) ) {
+			if ( ! preg_match( '/\?v=([^&]+)/', $src, $match ) ) {
+				if ( ! preg_match( '/https:\/\/youtu.be\/([^&]+)/', $src, $match ) ) {
+					return null;
+				}
+			}
+		}
+
+		$id = str_replace(["\r\n", "\r", "\n"], '', $match[1]);
+        _log('End getYoutubeId');
+        return $id;
+    }
+}
