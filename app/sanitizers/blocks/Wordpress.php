d2ee81a96bf32a1cf7ba803d4eaeb4042f2f339c
aphmau nejitsu <aphmau_nejitsu@icloud.com>
2020-12-06 07:59:17 +0900

    wordpress style

--- app/sanitizers/blocks/Wordpress.php
+++ app/sanitizers/blocks/Wordpress.php
@@ -30,20 +30,21 @@ class Wordpress
 
                 $permalink = get_permalink($postid);
                 $html = '<div class="container mumu-wordpress-card flex flex-column">';
-                $html .= '<header><h4 class="m1">' . esc_attr($post->post_title) . '</h4></header>';
                 if (has_post_thumbnail($postid)) {
                     $thum_id = get_post_thumbnail_id($postid);
-                    $image   = wp_get_attachment_image_src($thum_id, 'mumu-thumbnail-s-16x9');
+                    $image   = wp_get_attachment_image_src($thum_id, 'thumbnail');
 
                     $html   .= '<div class="card-thumbnail">';
-                    $html   .= '<a href="' . $permalink . '">';
-                    $html   .= '<amp-img src="' . $image[0] . '" width="' . $image[1] . '" height="' . $image[2] . '" layout="responsive" alt="' . esc_attr($post->post_title) . '">';
+                    $html   .= '<amp-img src="' . $image[0] . '" width="' . $image[1] . '" height="' . $image[2] . '" layout="fixed" alt="' . esc_attr($post->post_title) . '">';
                     $html   .= '</amp-img>';
-                    $html   .= '</a></div>';
+                    $html   .= '</div>';
                 }
 
                 $html .= '<div class="content">';
-                $html .= '<div class="body">' . mumu_excerpt($post->post_content) . '</div>';
+                $html .= '<div class="title">' . esc_attr($post->post_title) . '</div>';
+                $html .= '<div class="read-more flex justify-center my1">';
+                $html .= '<a href="' . $permalink . '" class="button">Read More</a>';
+                $html .= '</div>';
                 $html .= '</div>';
 
                 $html .= '</div>';
