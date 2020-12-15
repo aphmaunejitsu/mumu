4b2ed48c0fd5c846ee47b7adf430fb7f3dfd16ab
Aphamu Nejitsu <aphmaunejits@icloud.com>
2020-12-15 18:21:42 +0900

    use function_exists

--- app/filters.php
+++ app/filters.php
@@ -1,78 +1,87 @@
 <?php
 
-
 remove_filter('the_excerpt', 'wpautop');
 remove_filter('term_description', 'wpautop');
 add_filter('wp_lazy_loading_enabled', '__return_false');
 
-function newExcerptMore($more) {
-    return '...';
+if (! function_exists('mumu_new_excerpt_more')) {
+    function mumu_new_excerpt_more($more) {
+        return '...';
+    }
 }
-add_filter('excerpt_more', 'newExcerptMore');
+add_filter('excerpt_more', 'mumu_new_excerpt_more');
 
-function titleParts($title_parts)
-{
-    $title_parts['tagline'] = '';
-    $title_parts['site']    = '';
-    $site_name              = trim(get_bloginfo('name'));
-    if (is_front_page()) { //フロントページの場合
-        $title_parts['title']   = $site_name;
+if (! function_exists('mumu_title_parts')) {
+    function mumu_title_parts($title_parts)
+    {
+        $title_parts['tagline'] = '';
         $title_parts['site']    = '';
-        $title_parts['tagline'] = trim(get_bloginfo('description'));
-    } elseif (is_singular()) { //投稿ページの場合
-        $title_parts['title'] = trim(get_the_title());
-        $title_parts['site']  = $site_name;
-    } elseif (is_archive()) { //アーカイブページの場合
-        $title_parts['title'] = '「' . $title_parts['title'] . '」のアーカイブ';
-        $title_parts['site']  = $site_name;
-    } elseif (is_search()) {
-        $title_parts['title'] = $title_parts['title'];
-    } elseif (is_404()) { //404ページの場合
-        $title_parts['title'] = 'お探しのページは見つかりませんでした';
-        $title_parts['site']  = $site_name;
-    }
+        $site_name              = trim(get_bloginfo('name'));
+        if (is_front_page()) { //フロントページの場合
+            $title_parts['title']   = $site_name;
+            $title_parts['site']    = '';
+            $title_parts['tagline'] = trim(get_bloginfo('description'));
+        } elseif (is_singular()) { //投稿ページの場合
+            $title_parts['title'] = trim(get_the_title());
+            $title_parts['site']  = $site_name;
+        } elseif (is_archive()) { //アーカイブページの場合
+            $title_parts['title'] = '「' . $title_parts['title'] . '」のアーカイブ';
+            $title_parts['site']  = $site_name;
+        } elseif (is_search()) {
+            $title_parts['title'] = $title_parts['title'];
+        } elseif (is_404()) { //404ページの場合
+            $title_parts['title'] = 'お探しのページは見つかりませんでした';
+            $title_parts['site']  = $site_name;
+        }
 
-    return $title_parts;
+        return $title_parts;
+    }
 }
-add_filter('document_title_parts', 'titleParts');
+add_filter('document_title_parts', 'mumu_title_parts');
 
-function titleSeparator()
-{
-    $sep = ' | ';
-    return $sep;
+if (! function_exists('mumu_title_separator')) {
+    function mumu_title_separator()
+    {
+        $sep = ' | ';
+        return $sep;
+    }
 }
-add_filter('document_title_separator', 'titleSeparator');
+add_filter('document_title_separator', 'mumu_title_separator');
 
 
-function post_search($search)
-{
-    if (is_search()) {
-        $search .= " AND post_type = 'post'";
+if (! function_exists('mumu_post_search')) {
+    function mumu_post_search($search)
+    {
+        if (is_search()) {
+            $search .= " AND post_type = 'post'";
+        }
+        return $search;
     }
-    return $search;
 }
-add_filter('posts_search', 'post_search');
+add_filter('posts_search', 'mumu_post_search');
 
-function google_auto_adsens()
-{
-    $ad = null;
-    if (is_404()) {
-        return;
+if (! function_exists('mumu_google_auto_adsens_filter')) {
+    function mumu_google_auto_adsens_filter()
+    {
+        $ad = null;
+        if (is_404()) {
+            return;
+        }
+
+        if (! get_option('sgn_theme_ad_show')) {
+            return;
+        }
+
+        if (! get_option('sgn_theme_ad_adsens_auto')) {
+            return;
+        }
+
+        if (($adsens = get_option('sgn_theme_ad_adsens'))) {
+            $ad = '<amp-auto-ads type="adsense" data-ad-client="ca-pub-' . $adsens . "></amp-auto-ads>";
+        }
+
+        echo $ad;
     }
-
-    if (! get_option('sgn_theme_ad_show')) {
-        return;
-    }
-
-    if (! get_option('sgn_theme_ad_adsens_auto')) {
-        return;
-    }
-
-    if (($adsens = get_option('sgn_theme_ad_adsens'))) {
-        $ad = '<amp-auto-ads type="adsense" data-ad-client="ca-pub-' . $adsens . "></amp-auto-ads>";
-    }
-
-    echo $ad;
 }
-add_filter('google_auto_adsens', 'google_auto_adsens');
+add_filter('mumu_google_auto_adsens', 'mumu_google_auto_adsens_filter');
 
