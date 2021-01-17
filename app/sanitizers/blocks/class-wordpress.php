e235b7e7bbb74a1a25ea5684aa9b3d83cde15651
aphmaunejitsu <aphmau_nejitsu@icloud.com>
2021-01-17 19:34:28 +0900

    Change widget css of categories and recent entries

--- app/sanitizers/blocks/class-wordpress.php
+++ app/sanitizers/blocks/class-wordpress.php
@@ -37,9 +37,9 @@ class Wordpress extends BlockBase {
 				$permalink = get_permalink( $postid );
 				$html      = '<div class="container mumu-wordpress-card m1">';
 				if ( has_post_thumbnail( $postid ) ) {
-					$thum_id = get_post_thumbnail_id( $postid );
-					$image   = wp_get_attachment_image_src( $thum_id, 'thumbnail' );
-					$image   = wp_get_attachment_image_src( $thum_id, 'thumbnail' );
+					$thum_id   = get_post_thumbnail_id( $postid );
+					$image     = wp_get_attachment_image_src( $thum_id, 'thumbnail' );
+					$thumb_270 = wp_get_attachment_image_src( $thum_id, 'mumu-thumbnail-s-16x9' );
 
 					$html .= '<div class="card-thumbnail flex justify-center">';
 					$html .= '<a href="' . $permalink . '">';
@@ -51,8 +51,8 @@ class Wordpress extends BlockBase {
 
 				$html .= '<div class="content flex flex-column">';
 				$html .= '<div class="title">' . esc_html( $post->post_title ) . '</div>';
-				$html .= '<div class="excerpt">' . esc_html( mumu_excerpt( $post->post_content ), 70 ) . '</div>';
-				$html .= '<div class="read-more flex justify-end"><a href="' . $permalink . '">Read More</a></div>';
+				$html .= '<div class="excerpt">' . esc_html( mumu_excerpt( $post->post_content, 100 ) ) . '</div>';
+				$html .= '<div class="read-more flex justify-end mt-auto"><a href="' . $permalink . '">Read More</a></div>';
 				$html .= '</div>';
 				$html .= '</div>';
 
