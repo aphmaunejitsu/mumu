a41dc8ba416406be7ebf2b6e4b312d5be37687f8
aphmaunejitsu <aphmau_nejitsu@icloud.com>
2021-01-17 12:20:00 +0900

    Support embed wordpress url

--- app/sanitizers/blocks/class-wordpress.php
+++ app/sanitizers/blocks/class-wordpress.php
@@ -19,12 +19,13 @@ class Wordpress extends BlockBase {
 				$node = $this->nodes->item( $i );
 				$src  = $this->get_wordpress_src( $node );
 				if ( null === $src ) {
-					_log( 'nullpo WordPress' );
+					_log( 'nullpo WordPress: ' . $src );
 					continue;
 				}
 
 				$postid = url_to_postid( $src );
 				if ( ! $postid ) {
+					_log( 'nullpo WordPress id: ' . $src );
 					return $this->content;
 				}
 
@@ -34,13 +35,16 @@ class Wordpress extends BlockBase {
 				}
 
 				$permalink = get_permalink( $postid );
-				$html      = '<div class="container mumu-wordpress-card">';
+				$html      = '<div class="container mumu-wordpress-card m1">';
 				if ( has_post_thumbnail( $postid ) ) {
 					$thum_id = get_post_thumbnail_id( $postid );
+					$image   = wp_get_attachment_image_src( $thum_id, 'thumbnail' );
 					$image   = wp_get_attachment_image_src( $thum_id, 'thumbnail' );
 
 					$html .= '<div class="card-thumbnail flex justify-center">';
 					$html .= '<a href="' . $permalink . '">';
+					$html .= '<amp-img src="' . $image[0] . '" width="' . $image[1] . '" height="' . $image[2] . '" layout="fixed" alt="' . esc_html( $post->post_title ) . '">';
+					$html .= '</amp-img>';
 					$html .= '<amp-img src="' . $image[0] . '" width="' . $image[1] . '" height="' . $image[2] . '" layout="fixed" alt="' . esc_html( $post->post_title ) . '">';
 					$html .= '</amp-img>';
 					$html .= '</a>';
@@ -50,7 +54,7 @@ class Wordpress extends BlockBase {
 				$html .= '<div class="content flex flex-column">';
 				$html .= '<div class="title">' . esc_html( $post->post_title ) . '</div>';
 				$html .= '<div class="excerpt">' . esc_html( mumu_excerpt( $post->post_content ), 70 ) . '</div>';
-				$html .= '<div class="read-more m-auto"><a href="' . $permalink . '">Read More</a></div>';
+				$html .= '<div class="read-more flex justify-end"><a href="' . $permalink . '">Read More</a></div>';
 				$html .= '</div>';
 				$html .= '</div>';
 
@@ -72,14 +76,16 @@ class Wordpress extends BlockBase {
 	 * @param DOMNode $node DOMNode.
 	 */
 	public function get_wordpress_src( $node ) {
+		_log( 'Start: ' . __METHOD__ );
 		$source = $node->nodeValue;
 		$source = str_replace( array( "\r\n", "\r", "\n" ), '', $source );
 		if ( ! preg_match( '/https?:\/{2}[\w\/:%#\$&\?\(\)~\.=\+\-]+/', $source, $match ) ) {
 			return null;
 		}
 
-		_log( 'WordPress: ' . $match[0] );
+		_log( 'Match WordPress: ' . $match[0] );
 
+		_log( 'End: ' . __METHOD__ );
 		return $match[0];
 	}
 }
