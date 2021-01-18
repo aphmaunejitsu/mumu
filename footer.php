e54ae19d912c4320760edba3766933bad0667d87
aphmaunejitsu <aphmau_nejitsu@icloud.com>
2021-01-18 23:35:25 +0900

    fixed wrong link fb and twitter

--- footer.php
+++ footer.php
@@ -26,16 +26,18 @@
 		$mumu   = get_option( 'mumu' );
 		$tw_url = $mumu['theme_my_sns']['twitter'] ?? null;
 		$fb_url = $mumu['theme_my_sns']['fbpage'] ?? null;
+		_log( $tw_url );
+		_log( $fb_url );
 		if ( $tw_url ) :
 			?>
-			<a href="<?php esc_url( $tw_url ); ?>" target="_blank" class="inline-block p1" aria-label="<?php echo bloginfo( 'name' ); ?> Twitter">
+			<a href="<?php echo $tw_url; ?>" target="_blank" class="inline-block p1" aria-label="<?php echo bloginfo( 'name' ); ?> Twitter">
 			<?php get_template_part( 'parts/icons/svg', 'twitter' ); ?>
 			</a>
 			<?php
 		endif;
 		if ( $fb_url ) :
 			?>
-			<a href="<?php esc_url( $fb_url ); ?>" target="_blank" class="inline-block p1" aria-label="Link to <?php echo bloginfo( 'name' ); ?> Facebook">
+			<a href="<?php echo $fb_url; ?>" target="_blank" class="inline-block p1" aria-label="Link to <?php echo bloginfo( 'name' ); ?> Facebook">
 			<?php get_template_part( 'parts/icons/svg', 'facebook' ); ?>
 			</a>
 		<?php endif; ?>
